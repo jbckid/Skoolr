a060f43210dc36a905e7b6576a83d16368126b34
LG Rocket <toucannyc@gmail.com>
2012-01-09 15:06:02 -0500

    fresh ruby install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skoolr::Application.config.secret_token = 'ee5b1c7af91c1d79f0144505da82006efeda54c67f5bc37494f3d11fbdb56f4dff1ca814746e9c52065cbd74c989ac24bfa1c5d3b5d889cd7b8f150c78b0e161'
